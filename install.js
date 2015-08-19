9a536804de828ca4dfa41aeaf3e2be28c4f81dbb
kyrios.li <kyrios.li@qunar.com>
2015-08-19 10:55:38 +0800

    save

--- install.js
+++ install.js
@@ -1,12 +1,39 @@
-var uname = process.platform + '-' + process.arch + '-' + process.versions.modules + '.gz';
-
 var pkg = require('./package.json');
+var host = pkg.author + '.github.io';
+var options = {
+    method: 'GET',
+    host: host,
+    path: '/' + pkg.name + '/dists/' + process.platform + '-' + process.arch + '-' + process.versions.modules + '.gz',
+    headers: {
+        Host: host,
+        'Accept-Encoding': 'gzip, deflate',
+        Connection: 'close'
+    }
+};
 
-require('http').get('http://' + pkg.author + '.github.io/' + pkg.name + '/dists/' + uname, function(tres) {
-	console.log(tres.statusCode);
-}).on('error', onerror);
+require('http').request(options, function (tres) {
+    if (tres.statusCode !== 200) {
+        return onerror({message: 'file not found'});
+    }
+    var zlib = require('zlib');
+    if (tres.headers['content-encoding'] === 'gzip') {
+        tres = tres.pipe(zlib.createGunzip())
+    } else if (tres.headers['content-encoding'] === 'deflate') {
+        tres = tres.pipe(zlib.createInflateRaw())
+    }
+    tres = tres.pipe(zlib.createGunzip());
+    var fs = require('fs');
+    if (!fs.existsSync('build/Release')) {
+        fs.existsSync('build') || fs.mkdirSync('build');
+        fs.mkdirSync('build/Release');
+    }
+    tres.pipe(require('fs').createWriteStream('build/Release/java.node'));
+    tres.on('end', function () {
+        console.log('binary installed');
+    });
+}).on('error', onerror).end();
 
 function onerror(err) {
-	console.error(err.message);
-	console.error('binary installation failed, trying rebuild...');
+    console.error(err.message);
+    console.error('binary installation failed, please rebuild it manually...');
 }
